0bef22ed9071258526755141adb6c4fd6ad33a42
Vietdth <104123786+Tyrion1927@users.noreply.github.com>
2023-10-06 16:48:37 +0700

    Update loginformvalidation.js

--- js/loginformvalidation.js
+++ js/loginformvalidation.js
@@ -2,8 +2,8 @@
 	// form validation for login useremail: @jhu.edu
 	function submit_login(){
 		var loginname=document.loginform.loginname.value;
-		if (!(loginname.includes("@jhu.edu"))) {
-			alert("Please enter your @jhu.edu email.");
+		if (!(loginname.includes("@"))) {
+			alert("Please enter your email.");
 			document.loginform.loginname.value="";
 			document.loginform.loginname.focus();
 			return false;
@@ -13,8 +13,8 @@
 	// form validation for signup useremail
 	function submit_signup(){
 		var loginname=document.signupform.loginname.value;
-		if (!(loginname.includes("@jhu.edu"))) {
-			alert("Please enter your @jhu.edu email.");
+		if (!(loginname.includes("@"))) {
+			alert("Please enter your email.");
 			document.loginform.loginname.value="";
 			document.loginform.loginname.focus();
 			return false;
